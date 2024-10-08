f3d2992b4de56d9440903141abc794a5a5532936
Zajjaj Bin Sana <81627562+Zajjaj-Khan@users.noreply.github.com>
2024-10-09 00:52:04 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex mx-auto p-6  ">
+      <h1 className="font-bold text-xl">NextJS Starter Template</h1>
+    </div>
+  );
+}
